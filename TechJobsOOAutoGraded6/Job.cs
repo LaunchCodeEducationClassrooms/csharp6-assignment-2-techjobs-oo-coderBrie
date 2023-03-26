f562e02c0bdede671fef6250199d8b58505f38c7
Briaris Sayles <bri.sayles2@gmail.com>
2023-03-26 08:48:24 -0500

    added Job.cs unique ID and Job constructors. || built the Equals() and GetHashCode() methods to compare job.cs Id fields. || Vefrifed  all test 3 has passed

--- TechJobsOOAutoGraded6/Job.cs
+++ TechJobsOOAutoGraded6/Job.cs
@@ -3,7 +3,7 @@ namespace TechJobsOOAutoGraded6
 {
 	public class Job
 	{
-        /*
+        ///*
 
             public int Id { get; }
             private static int nextId = 1;
@@ -15,13 +15,42 @@ namespace TechJobsOOAutoGraded6
 
             // TODO: Task 3: Add the two necessary constructors.
 
-            // TODO: Task 3: Generate Equals() and GetHashCode() methods.  
+         public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
 
-            // TODO: Task 5: Generate custom ToString() method.
-                //Until you create this method, you will not be able to print a job to the console.
+        
+        public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) : this()
+        {
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            JobType = jobType;
+            JobCoreCompetency = jobCoreCompetency;
+        }
 
+        // TODO: Task 3: Generate Equals() and GetHashCode() methods.
+        //
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !obj.GetType().Equals(this.GetType()))
+            {
+                return false;
+            }
+            Job otherJob = (Job)obj;
+            return Id == otherJob.Id;
+        }
 
-        */
+        // TODO: Task 5: Generate custom ToString() method.
+        //Until you create this method, you will not be able to print a job to the console.
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
     }
 }
 
