46a438049669bb5712960f5995afb0496b7d2de3
Briaris Sayles <bri.sayles2@gmail.com>
2023-03-27 19:11:18 -0500

    add .equals and verified all TestTasks4 are passing .

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -1,19 +1,18 @@
 ﻿
 namespace TechJobs.Tests
 {
-	[TestClass]
-	public class JobTests
-	{
+    [TestClass]
+    public class JobTests
+    {
         //Testing Objects
         //initalize your testing objects here
 
         [TestMethod]
         public void TestMethod()
         {
-            //    //TODO: Task 4: remove this method before you create your first test method 
-            //
 
-            //---------------------------------------------------------------------------------------------------------------------------/
+
+
 
             Job job1 = new Job();
 
@@ -23,11 +22,53 @@ namespace TechJobs.Tests
 
             Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
-            //---------------------------------------------------------------------------------------------------------------------------//
+
             Assert.IsFalse(job1.Id == job2.Id);
             Assert.AreEqual(job1.Id + 1, job2.Id);
 
         }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            // Arrange
+            Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            // Act & Assert
+            Assert.AreEqual("Product tester", job.Name);
+            Assert.AreEqual("ACME", job.EmployerName.Value);
+            Assert.AreEqual("Desert", job.EmployerLocation.Value);
+            Assert.AreEqual("Quality control", job.JobType.Value);
+            Assert.AreEqual("Persistence", job.JobCoreCompetency.Value);
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            // Arrange
+            Job job1 = new Job("Software Engineer", new Employer("ABC Company"), new Location("Seattle"), new PositionType("Full-Time"), new CoreCompetency("Coding"));
+            Job job2 = new Job("Data Scientist", new Employer("XYZ Inc."), new Location("San Francisco"), new PositionType("Full-Time"), new CoreCompetency("Data Analysis"));
+
+            // Act
+            bool result = job1.Equals(job2);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        public void TestSettingJobId()
+        {
+            // Arrange
+            Job job = new Job();
+            int expectedId = 1;
+
+            // Act
+            int actualId = job.Id;
+
+            // Assert
+            Assert.AreEqual(expectedId, actualId);
+        }
+
     }
 }
 
