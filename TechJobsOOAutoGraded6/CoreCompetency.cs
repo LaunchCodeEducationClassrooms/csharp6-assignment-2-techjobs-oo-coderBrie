6957d26d2d429f9a1cad2a0fc60ea6e422e4e7a6
Briaris Sayles <bri.sayles2@gmail.com>
2023-04-06 20:04:55 -0500

    built and rebuilt the refactored code, fixed thrown errors and syntax error, all tests passing…

--- TechJobsOOAutoGraded6/CoreCompetency.cs
+++ TechJobsOOAutoGraded6/CoreCompetency.cs
@@ -1,40 +1,15 @@
 ﻿using System;
 namespace TechJobsOOAutoGraded6
 {
-	public class CoreCompetency
-	{
-        public int Id { get; }
-        private static int nextId = 1;
-        public string Value { get; set; }
+    public class CoreCompetency : JobField
+    {
+        
 
-        // TODO: Task 2: Change the fields to auto-implemented properties.
-
-        public CoreCompetency()
+        public CoreCompetency(string value) : base(value)
         {
-            Id = nextId;
-            nextId++;
-        }
 
-        public CoreCompetency(string value) : this()
-        {
-            Value = value;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is CoreCompetency competency &&
-                   Id == competency.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
-
-        public override string ToString()
-        {
-            return Value;
         }
     }
+
 }
 
